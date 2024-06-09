ca412bd1e9b3c06837b6cb42f07d82e5a35104c6
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2024-06-09 10:58:09 +0200

    2-snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,65 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import imageUrl from '../img/alert-icon.svg'
+import successImageUrl from '../img/succes-img.svg';
+
+const inputEl = document.querySelector('.form');
+console.log(inputEl);
+
+
+inputEl.addEventListener('submit', promiseFoo);
+
+function promiseFoo(e) {
+  e.preventDefault();
+
+  const delay = e.target.delay.value;
+  const status = e.target.state.value
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay)
+      }
+    }, delay);
+  });
+
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `Fulfilled promise in ${delay}ms`,
+        messageSize: '16',
+        messageColor: '#fff',
+        backgroundColor: '#59a10d',
+        position: 'topRight',
+        close: true,
+        closeOnEscape: true,
+        closeOnClick: true,
+        progressBar: true,
+        progressBarColor: '#326101',
+        iconColor: '#fff',
+        iconUrl: successImageUrl,
+      });
+      
+    })
+    .catch(delay => {
+      iziToast.error({
+  message: `Rejected promise in ${delay}ms`,
+  messageSize: '16',
+  messageColor: '#fff',
+  backgroundColor: '#ef4040',
+  position: 'topRight',
+  close: true,
+  closeOnEscape: true,
+  closeOnClick: true,
+  progressBar: true,
+  progressBarColor: '#ffbebe',
+  iconUrl: imageUrl,
+  iconColor: '#fff',
+});
+    });
+  
+  console.log(promise);
+};
